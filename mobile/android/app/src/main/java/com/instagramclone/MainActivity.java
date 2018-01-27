2c6ffa63ac111236da9608a9766a95f50df8a0b1
Emanuel Quimper <quimperemanuel@gmail.com>
2018-01-27 16:22:08 -0500

    Add fbsdk for android

--- mobile/android/app/src/main/java/com/instagramclone/MainActivity.java
+++ mobile/android/app/src/main/java/com/instagramclone/MainActivity.java
@@ -1,7 +1,13 @@
 package com.instagramclone;
 
 import com.reactnativenavigation.controllers.SplashActivity;
+import android.content.Intent;
 
 public class MainActivity extends SplashActivity {
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
 }
--- mobile/android/app/src/main/java/com/instagramclone/MainApplication.java
+++ mobile/android/app/src/main/java/com/instagramclone/MainApplication.java
@@ -5,11 +5,19 @@ import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
+import com.facebook.CallbackManager;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends NavigationApplication {
+
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+    protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
+    }
 
     @Override
     public boolean isDebug() {
@@ -22,7 +30,8 @@ public class MainApplication extends NavigationApplication {
         // No need to add RnnPackage and MainReactPackage
         return Arrays.<ReactPackage>asList(
                 new VectorIconsPackage(),
-                new LinearGradientPackage()
+                new LinearGradientPackage(),
+                new FBSDKPackage(mCallbackManager)
         );
     }
 
