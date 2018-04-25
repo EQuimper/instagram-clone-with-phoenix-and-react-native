7b7e017800eaa9d738001890000a6cb8fb021b2a
Emanuel Quimper <quimperemanuel@gmail.com>
2018-04-24 20:32:14 -0400

    end m05-ep05

--- mobile/src/components/index.js
+++ mobile/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as PhotoCard } from './PhotoCard';
 export { default as Divider } from './Divider';
 export { default as Comment } from './Comment';
+export { default as ListSpacer } from './ListSpacer';
